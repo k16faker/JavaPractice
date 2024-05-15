bca77be4c8135e8cd08f1f7cf2cc894806110b86
ParkSeongJun <sjyj98@naver.com>
2024-05-15 10:49:50 +0900

    5/15 interface

---
+++ object-oriented-programming-2/src/com/in28minutes/oops/level2/interfaces/MarioGame.java
@@ -0,0 +1,29 @@
+package com.in28minutes.oops.level2.interfaces;
+
+public class MarioGame implements GamingConsole {
+
+	@Override
+	public void up() {
+		System.out.println("Jump");
+
+	}
+
+	@Override
+	public void down() {
+		System.out.println("Goes into a hole");
+
+	}
+
+	@Override
+	public void left() {
+		System.out.println("go left");
+
+	}
+
+	@Override
+	public void right() {
+		System.out.println("go right");
+
+	}
+
+}
